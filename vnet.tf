9ad1b8eebdf788970a23d03e4b6b36fb2533c6bd
rkumar <rkumar@xactlycorp.com>
2021-05-13 18:54:55 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -1,6 +1,11 @@
 module "network" {
   source  = "app.terraform.io/ravikumar-training/network/azurerm"
   version = "3.0.1"
-  resource_group_name = "gaurav"
   # insert required variables here
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "my-resources"
+  location = "West Europe"
 }
